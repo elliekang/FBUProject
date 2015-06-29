7e8daa51d28d5844489d3758f5339d67bb95b4cc
Ellie Kang <elliekang@elliekang-mbp.dhcp.thefacebook.com>
2015-06-29 15:16:56 -0700

    parse added

--- fbuproject/app/src/main/java/teaminnovate/fbuproject/MainActivity.java
+++ fbuproject/app/src/main/java/teaminnovate/fbuproject/MainActivity.java
@@ -1,9 +1,12 @@
 package teaminnovate.fbuproject;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "Zck6PZxVCHpwNJAbDnf5KTAohqTClXg388uF3maY", "s5tD1Ls2CkwBy1oJCRwRnfLstUVYv6zilWlumNrf");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 
     @Override
